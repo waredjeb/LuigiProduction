ea4d72cc1890751b444af3009e76139473a4d497
â <bruno.alves@cern.ch>
2021-12-02 22:45:50 +0100

    add --full option

--- triggerClean.sh
+++ triggerClean.sh
@@ -2,12 +2,14 @@
 
 help_description="prints this help message"
 tag_description="select tag"
+full_description="also removes HTCondor outputs"
 debug_description="debug: prints everything, runs nothing"
 function print_usage_triggerClean {
     usage=" $(basename "$0") [-h] [-t -d]: removes folders
 where:
     -h / --help  [ ${help_description} ]
     -t / --tag   [ ${tag_description} ]
+    -f / --full  [ ${full_description} ]
     -d / --debug [ ${debug_description} ]
 
     Run example: $(basename "$0") -t v1
@@ -16,6 +18,7 @@ where:
 }
 
 DEBUG=false
+FULL=false
 while [[ $# -gt 0 ]]; do
     key="$1"
 
@@ -28,6 +31,10 @@ while [[ $# -gt 0 ]]; do
 	    TAG="$2"
 	    shift # past argument
 	    shift # past value
+	    ;;
+	-f|--full)
+	    FULL=true
+	    shift # past argument
 	    ;;
 	-d|--debug)
 	    DEBUG=true
@@ -48,12 +55,15 @@ fi
 echo "####### Arguments #####################"
 echo "TAG   = ${TAG}"
 echo "DEBUG = ${DEBUG}"
+echo "FULL = ${DEBUG}"
 echo "#######################################"
 
 ##### FUNCTION DEFINITION #####
 declare -a COMMANDS=( "rm -rf /eos/home-b/bfontana/www/TriggerScaleFactors/${TAG}/*"
-		      "rm -rf /data_CMS/cms/alves/TriggerScaleFactors/${TAG}/"
 		      "rm -rf jobs/${TAG}/" )
+if $FULL; then
+    COMMANDS+=( "rm -rf /data_CMS/cms/alves/TriggerScaleFactors/${TAG}/" )
+fi
 
 for comm in "${COMMANDS[@]}"; do
     if $DEBUG; then
